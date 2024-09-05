b4376a1d15f6674fb4642d88f3bf1893d11d35fe
Abdelrahman-Lila <abdelrahman.lila@ejust.edu.eg>
2024-09-05 19:58:29 +0300

    The client and server are now sending and receiving to each other

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,6 +7,21 @@ PORT = 9000
 ADDRESS = "localhost"
 my_socket.connect((ADDRESS, PORT))
 
-while True:
-    message_to_send = input("Enter your message: ")
-    my_socket.send(message_to_send.encode())
+
+def thread_sending():
+    while True:
+        message_to_send = input("Enter your message: ")
+        my_socket.send(message_to_send.encode())
+
+
+def thread_receiving():
+    while True:
+        message = my_socket.recv(1024)
+        print(message.decode())
+
+
+thread_send = threading.Thread(target=thread_sending)
+thread_receive = threading.Thread(target=thread_receiving)
+
+thread_send.start()
+thread_receive.start()
