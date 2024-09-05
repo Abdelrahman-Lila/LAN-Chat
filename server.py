4520a4e9bd9bf26477deeab0dc3340ae3ac512a9
Abdelrahman-Lila <abdelrahman.lila@ejust.edu.eg>
2024-09-05 19:28:09 +0300

    First Basic Server-Client Implementation

---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+PORT = 9000
+ADDRESS = "0.0.0.0"
+my_socket.bind((ADDRESS, PORT))
+
+my_socket.listen()
+client, client_address = my_socket.accept()
+
+message = client.recv(1024)
+
+print(message.decode())
